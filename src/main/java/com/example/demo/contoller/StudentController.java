6b7ddae6270a6a57d1912bf06f298266ee630e96
uswarna123 <ushaswarna@123>
2022-07-26 14:40:22 -0400

    new REST methods added

--- src/main/java/com/example/demo/contoller/StudentController.java
+++ src/main/java/com/example/demo/contoller/StudentController.java
@@ -1,9 +1,59 @@
 package com.example.demo.contoller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.Course;
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController
 {
+    @Autowired
+    StudentRepository studentRepository;
 
+    @GetMapping("/Hello")
+    public String hell(){
+        System.out.println("Tested ok");
+        return "Tested Ok";
+    }
+   @GetMapping("/findAllStudents")
+    public List<Student> findAllStudents(){
+        return studentRepository.findAll();
+    }
+    @PostMapping("/addStudent")
+    public ResponseEntity<Student> addNewStudent(@RequestBody Student student){
+        Student response=studentRepository.save(student);
+        return ResponseEntity.ok(response);
+    }
+
+     @PutMapping("/UpdateStudent/{id}")
+     public ResponseEntity<Student> updateStudent(@RequestBody Student student,@PathVariable(name = "id")Long Id){
+         Student studentResponse=studentRepository.findById(Id).get();
+         studentResponse.setStudentName(student.getStudentName());
+         studentResponse.setCourse(student.getCourse());
+         Student updatedStudent = studentRepository.save(studentResponse);
+         return ResponseEntity.ok(updatedStudent);
+     }
+
+     @PatchMapping("/ModifyStudent/{Id}")
+     public ResponseEntity<Student> modifyStudent(@RequestBody Student student,@PathVariable(name = "Id") Long Id){
+         Student studentResponse = studentRepository.findById(Id).get();
+         studentResponse.setStudentName(student.getStudentName());
+         studentResponse.setCourse(student.getCourse());
+         Student modifiedStudent = studentRepository.save(studentResponse);
+         return ResponseEntity.ok(modifiedStudent);
+     }
+
+
+    @DeleteMapping("/DeleteAllStudents")
+    public String deleteAllStudent(){
+        studentRepository.deleteAll();
+        return "All Student Records Deleted";
+    }
 }
--- src/main/java/com/example/demo/data/CourseDataTableLoader.java
+++ src/main/java/com/example/demo/data/CourseDataTableLoader.java
@@ -16,14 +16,32 @@ public class CourseDataTableLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-      //addCourseData();
+    // addCourseData();
+    //   addCourseData2();
+   //  addCourseData1();
     }
 
     public void addCourseData()
     {
         Course course = new Course();
-        course.setName("Hadoop");
-        course.setDuration("3 Months");
+        course.setName("Payton");
+        course.setDuration("1 Year");
+        courseRepository.save(course);
+        System.out.println("Data inserted in Course table");
+    }
+    public void addCourseData1()
+    {
+        Course course = new Course();
+        course.setName("C++");
+        course.setDuration("6 Months");
+        courseRepository.save(course);
+        System.out.println("Data inserted in Course table");
+    }
+    public void addCourseData2()
+    {
+        Course course = new Course();
+        course.setName("Java");
+        course.setDuration("12 Months");
         courseRepository.save(course);
         System.out.println("Data inserted in Course table");
     }
